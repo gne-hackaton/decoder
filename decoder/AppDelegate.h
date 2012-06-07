2d43c32e0fd980a31b684f11ba89648acc27ace2
Maciej Skolecki <maciej_skolecki@yahoo.com>
2012-06-07 14:59:37 -0700

    initial commit

---
+++ decoder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  decoder
+//
+//  Created by Maciej Skolecki on 6/7/12.
+//  Copyright (c) 2012 Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ decoder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  decoder
+//
+//  Created by Maciej Skolecki on 6/7/12.
+//  Copyright (c) 2012 Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
