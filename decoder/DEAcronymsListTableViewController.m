09435c8b917cb10d4bfc0c52ecf9a29aa5424ccd
YM <mayuliang@gmail.com>
2012-06-07 22:51:19 -0700

    bug fixes

--- decoder/DEAcronymsListTableViewController.m
+++ decoder/DEAcronymsListTableViewController.m
@@ -3,10 +3,25 @@
 #import "DEAcronym.h"
 #import "DEJsonRequest.h"
 
+#define SEARCH_URL @"http://10-36-209-202.wifi.gene.com:4567/search/"
+
 @implementation DEAcronymsListTableViewController
 
-@synthesize listContent, savedSearchTerm, searchWasActive;
+@synthesize listContent = _listContent, tableView = _tableView;
 
+- (NSArray *)listContent {
+    if (!_listContent) {
+        _listContent = [[NSArray alloc] init];
+    }
+    return _listContent;
+}
+
+- (void) setListContent:(NSArray *)listContent {
+    if (listContent != _listContent) {
+        _listContent = listContent;
+        [self.tableView reloadData];
+    }
+}
 
 #pragma mark - 
 #pragma mark Lifecycle methods
@@ -14,36 +29,14 @@
 - (void)viewDidLoad
 {
 	self.title = @"DECODER RING";
-    
-    if (!self.listContent) {
-        self.listContent = [[NSArray alloc] init];
-    }
 	
-	// restore search settings if they were saved in didReceiveMemoryWarning.
-    if (self.savedSearchTerm)
-	{
-        [self.searchDisplayController setActive:self.searchWasActive];
-
-        [self.searchDisplayController.searchBar setText:savedSearchTerm];
-        
-        self.savedSearchTerm = nil;
-    }
-	
-	[self.tableView reloadData];
 	self.tableView.scrollEnabled = YES;
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    // save the state of the search UI so that it can be restored if the view is re-created
-    self.searchWasActive = [self.searchDisplayController isActive];
-    self.savedSearchTerm = [self.searchDisplayController.searchBar text];
-
 }
 
 - (void)dealloc
 {
-	[listContent release];
+	[_listContent release];
+    [_tableView release];
 	
 	[super dealloc];
 }
@@ -54,10 +47,8 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    NSLog(@"numberOfRowsInSection: %d", [self.listContent count]);
     return [self.listContent count];
 }
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -71,7 +62,7 @@
 	}
 
 	DEAcronym *acronym = nil;
-    NSLog(@"indexPath.row: %d", indexPath.row);
+
 	acronym = [self.listContent objectAtIndex:indexPath.row];
 	
 	cell.textLabel.text = acronym.name;
@@ -92,39 +83,40 @@
 	[detailsViewController release];
 }
 
-- (void)searchWithSearchText: (NSString *)searchText {
+- (void)searchWithSearchTerm: (NSString *) searchText {
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
     
-    NSString *searchURL = @"http://10-36-209-202.wifi.gene.com:4567/search/";
+    NSString *searchURL = SEARCH_URL;
     NSString *searchString = [searchURL stringByAppendingString:[searchText capitalizedString]];
     DEJsonRequest *r = [[DEJsonRequest alloc] initWithURL:searchString];
     [r connect];
     
 	r.completion = ^(id data) {
-//		NSLog(@"acronyms array: %@", data);
+		NSLog(@"acronyms array: %@", data);
         self.listContent = data;
-        [self.tableView reloadData];
         [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
 	};
     [r release];
 }
 
 #pragma mark -
-#pragma mark UISearchDisplayController Delegate Methods
+#pragma mark UISearchBar Delegate Methods
 
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
-    NSLog(@"searchBarSearchButtonClicked");
     [self.view endEditing:TRUE];
-    [self searchWithSearchText:searchBar.text];
-    
 }
 
 - (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
     NSLog(@"searchBarTextDidEndEditing");
 }
+
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
-    NSLog(@"textDidChange: %@", searchText);
-    [self searchWithSearchText:searchText];
+    [self searchWithSearchTerm:searchText];
+}
+
+#pragma mark - UIScrollView Delegate Methods
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    [self.view endEditing:TRUE];
 }
 
 @end
