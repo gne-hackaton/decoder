7259f65a15b7181e61ebdd539f9ea4db609e172f
Kamil Kocemba <kkocemba@gmail.com>
2012-06-13 12:42:42 -0700

    Changed MIME type to application/json

--- AwsomeSearchEngine/AwsomeSearchEngine.rb
+++ AwsomeSearchEngine/AwsomeSearchEngine.rb
@@ -3,9 +3,14 @@ require 'sinatra'
 require 'json'
 require 'redis'
 
+configure do
+        mime_type :json, "application/json"
+end
+
 get '/search/:prefix' do
 	redis = Redis.new(:host => "127.0.0.1", :port => 6379)
 	keys = redis.keys "#{params[:prefix]}*".upcase
+	content_type :json
 	result = "{\"result\":[";
 	keys.each do |key|
 		result += redis.get(key)
